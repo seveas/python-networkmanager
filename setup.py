9cc8ce6e907c1db2be669800198fd9c6baf0f169
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2011-11-04 09:52:27 +0100

    Align version number with NetworkManager itself

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "python-networkmanager",
-      version = "1.0",
+      version = "0.9.1",
       author = "Dennis Kaarsemaker",
       author_email = "dennis@kaarsemaker.net",
       url = "http://github.com/seveas/python-networkmanager",
