8f17d809f2ff05bfa745df4c205fd6124145611f
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2015-03-04 16:55:16 +0100

    Add an example that shows all visible SSID's
    Based on a StackOverflow question

---
+++ examples/ssids.py
@@ -0,0 +1,11 @@
+"""
+Display all visible SSIDs
+"""
+
+import NetworkManager
+
+for dev in NetworkManager.NetworkManager.GetDevices():
+    if dev.DeviceType != NetworkManager.NM_DEVICE_TYPE_WIFI:
+        continue
+    for ap in dev.SpecificDevice().GetAccessPoints():
+        print '%-30s %dMHz %d%%' % (ap.Ssid, ap.Frequency, ap.Strength)
